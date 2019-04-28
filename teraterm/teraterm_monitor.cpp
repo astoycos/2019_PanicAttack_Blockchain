1e4e9fb47e937f82624fdceffb043002c77af12a
Eric Li <ericli21@bu.edu>
2019-04-28 11:57:57 -0400

    general comments

--- teraterm/teraterm_monitor.cpp
+++ teraterm/teraterm_monitor.cpp
@@ -5,34 +5,37 @@
 
 int main()
 {
+    //Monitor teraterm.log
     std::ifstream ifs("teraterm.log");
     if (ifs.is_open())
     {
         std::string line;
         while (true)
         {
+            //Get line if possible
             if (std::getline(ifs,line)) {
+                //Remove old data file and make a new one
                 remove( "data.txt" );
                 std::ofstream ofs("data.txt", ios::app);
                 if (line != "\n" && line != "") {
-
                     std::cout << line << "\n";
                     ofs << line << "\n";
                 }
+                //Repeat for the rest of the lines
                 while (std::getline(ifs, line)) {
                     if (line != "\n" && line != "") {
-
                     std::cout << line << "\n";
                     ofs << line << "\n";
                     }
                 }
-                std::system("./test_secondfunction");
+                //System to go function (if needed)
+                //std::system("go run ../GO_blockchain/main.go");
                 std::cout << "Done\n"; 
             }
-            
+            // Break when EOF
             if (!ifs.eof()) {
                 break;
-            } // Ensure end of read was EOF.
+            } 
             ifs.clear();
             sleep(1);
         }
